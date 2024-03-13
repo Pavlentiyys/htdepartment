0ee602dbd3a3889e5eb26cdaa878e8863e9b3ba4
N1tru17 <pashpichug@mail.ru>
2024-03-13 12:28:27 +0600

    added new modal to execute

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,16 +8,18 @@ import checkSvgPrimary from '../images/check_icon_primary.svg';
 import checkSvgNormal from '../images/check_icon_normal.svg';
 import Dropdown from './choice/Choice.jsx';
 import EditPost from './EditPost';
+import Input from './input/Input.jsx';
 
 export default function Post({ selectedArticle }) {
     const [isEditing, setIsEditing] = useState(false);
     const [isExtending, setIsExtending] = useState(false);
     const [isSelectingDepartment, setIsSelectingDepartment] = useState(false);
+    const [isExecuting, setIsExecuting] = useState(false); // Добавленное состояние
 
     const startEditing = () => {
         setIsEditing(true);
     };
-    
+
     const stopEditing = () => {
         setIsEditing(false);
     };
@@ -29,12 +31,21 @@ export default function Post({ selectedArticle }) {
     const stopExtending = () => {
         setIsExtending(false);
     };
+
     const startSelectingDepartment = () => {
         setIsSelectingDepartment(true);
     };
 
     const stopSelectingDepartment = () => {
         setIsSelectingDepartment(false);
+    };
+
+    const startExecuting = () => {
+        setIsExecuting(true); // Функция для начала выполнения
+    };
+
+    const stopExecuting = () => {
+        setIsExecuting(false); 
     };
 
     return (
@@ -103,6 +114,7 @@ export default function Post({ selectedArticle }) {
                         </div>
                     </section>
                 )}
+
                 {/* Модальное окно для выбора другого отдела */}
                 {isSelectingDepartment && (
                     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
@@ -137,7 +149,7 @@ export default function Post({ selectedArticle }) {
                             </div>
                             <div className='flex:none lg:flex justify-center gap-5'>
                                 <div className='w-full lg:w-1/3'>
-                                    <Button color="Primary" icon={checkSvgPrimary}>Выполнить</Button>
+                                    <Button color="Primary" onClick={startExecuting} icon={checkSvgPrimary}>Выполнить</Button>
                                 </div>
                                 <div className='w-full lg:w-1/3'>
                                     <Button onClick={startExtending}>Продлить</Button>
@@ -145,10 +157,29 @@ export default function Post({ selectedArticle }) {
                             </div>
                         </div>
                     </section>
-                    
                 )}
+                {/* По нажатию кнопки выполнить появляется модальное окно с добавлением фотоотчёта */}
                 <section>
+
+                    {isExecuting && (
+                        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
+                            <div className="bg-white p-4 md:p-8 rounded-lg w-96 md:w-2/3 max-w-lg">
+                                <h2 className="text-lg md:text-xl font-semibold mb-4">Выполнить задачу</h2>
+                                <Input type="image"><span className='font-medium'>Фотоотчёт</span></Input>
+                                <div className="flex:none md:flex justify-end gap-3 mt-4">
+                                    <div className='w-full'>
+                                        <Button color="Primary" onClick={stopExecuting}>Подтвердить</Button>
+                                    </div>
+                                    <div className='w-full'>
+                                        <Button onClick={stopExecuting}>Отмена</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </section>
                 {/* Модальное окно продления работы*/}
+                <section>
                 {isExtending && (
                             <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
                                 <div className="bg-white p-4 md:p-8 rounded-lg w-96 md:w-2/3 max-w-lg">
